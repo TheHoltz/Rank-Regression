80e9bca4a54989988ec55ebb46c60982143a93e5
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-06-14 21:52:57 -0300

    Update Tutorial.r

--- Tutorial.r
+++ Tutorial.r
@@ -1,12 +1,12 @@
 #Universidade federal de minas gerais
 #William Giani Duani Martins
 
-# importei
+# importing libraries and a dataset
 require(dplyr)
 require(tidyverse)
 dados <- read.csv2("https://pastebin.com/raw/Pxi5p3Ap", header=T)
 
-# limpei
+# cleaning some values
 dados <- dados[1:4,1:2]
 
 combinations <- combn(c(1:4),2)
@@ -16,7 +16,7 @@ int <- c()
 inc <- c()
 pesos <- c()
 
-# Calculando todas as possíveis regressões
+# Calculating all possible regressions
 for(i in 1:6){
   y <- rbind(dados[combinations[,i][1],][1],dados[combinations[,i][2],][1]) %>% as.matrix()
   x <- cbind(c(1,1), rbind(dados[combinations[,i][1],][2],dados[combinations[,i][2],][2])) %>% as.matrix()
@@ -25,31 +25,31 @@ for(i in 1:6){
   inc <- rbind(inc, solve(x,y)[2])
 }
 
-# montando tabela auxiliar
+# creating an auxiliary table
 tabelaAuxiliar <- c()
 tabelaAuxiliar <- cbind(int,inc,pesos)
 tabelaAuxiliar
 
-# convertendo distancias em pesos
+# converting the distances in weights
 tabelaAuxiliarConvertida <- tabelaAuxiliar %>% arrange(inc) 
 tabelaAuxiliarConvertida <- cbind(tabelaAuxiliarConvertida, cumsum(pesos/sum(pesos)))
 colnames(tabelaAuxiliarConvertida) <- c("int","inc","distancia","pesoacum")
 
-#pegando a mediana
+# extracting the median
 filter(tabelaAuxiliarConvertida, pesoacum>0.5)[1,]
 beta1 <- filter(tabelaAuxiliarConvertida, pesoacum>0.5)[1,2]
 
 
-# calculando betas0
+# calculating all betas0
 betas0 <- c()
 for(i in 1:4){
   betas0 <- rbind(betas0, dados[i,][1] - beta1 * dados[i,][2])
 }
 
-# mediana dos betas0
+# extracting betas0 median
 betas0 <- betas0 %>% arrange(y)
 beta0 <- round(median(betas0$y), 2)
 
-# resultados
+# final results
 beta0
 beta1
